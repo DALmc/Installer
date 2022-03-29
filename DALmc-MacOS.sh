71e32910529b7ab108e58426daf8db84159a1146
gemsvidø <gemsvido@gmail.com>
2022-03-28 21:50:21 -0700

    DALmc Modpack Installer

---
+++ DALmc-MacOS.sh
@@ -0,0 +1,23 @@
+echo [DALmc] MacOS DALmc Modpack installer
+
+# if [ -d "/Users/%USER%/Library/Application Support/minecraft" ] fi
+# if no .minecraft folder, then no install.
+
+
+
+
+echo [DALmc] Cloning the mods repository...
+git clone https://github.com/DALmc/mods.git
+echo [DALmc] Cloned the mods repository.
+
+
+
+# echo [DALmc] Cloning the config repository...
+# git clone https://github.com/DALmc/config.git
+# echo [DALmc] Cloned the config repository.
+
+
+echo [DALmc] Succesfully installed the DALmc modpack!
+echo [DALmc] Done!
+
+exit 0
